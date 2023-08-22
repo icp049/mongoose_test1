16b5ffd538ac9c5709e77f378d99d8490e911e4a
icp049 <ianpepe2000@gmail.com>
2023-08-22 12:49:08 -0600

    restarted server.js

--- src/server.js
+++ src/server.js
@@ -1,12 +1,5 @@
 require('dotenv').config(); // Load environment variables from .env file
 
-const crypto = require("crypto");
-
-const generateSecretKey = () => {
-  return crypto.randomBytes(32).toString("hex");
-};
-
-const secretKey = generateSecretKey();
 
 
 
@@ -17,9 +10,6 @@ const multer = require("multer");
 const { Readable } = require("stream");
 
 
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const session = require("express-session");
 
 const app = express();
 const port = 3000;
@@ -104,50 +94,7 @@ app.post("/register", upload.single("avatar"), async (req, res) => {
 });
 
 
-app.use(session({ secret: secretKey, resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
 
-// Passport local strategy configuration
-passport.use(new LocalStrategy(
-  { usernameField: "email" },
-  async (email, password, done) => {
-    try {
-      const user = await User.findOne({ email });
-
-      if (!user) {
-        return done(null, false, { message: "Incorrect email or password" });
-      }
-
-      if (user.password !== password) {
-        return done(null, false, { message: "Incorrect email or password" });
-      }
-
-      return done(null, user);
-    } catch (err) {
-      return done(err);
-    }
-  }
-));
-
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await User.findById(id);
-    done(null, user);
-  } catch (err) {
-    done(err);
-  }
-});
-
-// Express route for user login
-app.post("/login", passport.authenticate("local", {
-  successRedirect: "/landingpage", // Redirect to the landing page on successful login
-  failureRedirect: "/", // Redirect back to login page on failure
-}));
 
 server.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);

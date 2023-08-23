26534ccd245243bc9228b97b382e698c5cc77f90
icp049 <ianpepe2000@gmail.com>
2023-08-23 00:50:13 -0600

    deleted passport from usermodel

--- src/UserModel.js
+++ src/UserModel.js
@@ -1,6 +1,6 @@
 // UserModel.js
 const mongoose = require("mongoose");
-const passportLocalMongoose = require("passport-local-mongoose");
+
 
 const userSchema = new mongoose.Schema({
   displayName: String,
@@ -8,7 +8,6 @@ const userSchema = new mongoose.Schema({
   photoURL: String,
 });
 
-userSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model("User", userSchema);
 

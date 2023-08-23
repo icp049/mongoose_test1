071f6300bdd41677148f44fdec1b8b7e6c8fe9c6
icp049 <ianpepe2000@gmail.com>
2023-08-22 21:23:46 -0600

    modified my external user model

--- src/UserModel.js
+++ src/UserModel.js
@@ -1,11 +1,14 @@
 // UserModel.js
 const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const userSchema = new mongoose.Schema({
   displayName: String,
   email: String,
   photoURL: String,
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model("User", userSchema);
 

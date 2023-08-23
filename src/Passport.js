196aa69e818d11519c95970907a17ca8bd6a44c2
icp049 <ianpepe2000@gmail.com>
2023-08-22 21:22:37 -0600

    modified passport again

--- src/Passport.js
+++ src/Passport.js
@@ -1,9 +1,7 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-const mongoose = require("mongoose");
 
-// Import the UserModel schema directly (if defined in server.js)
-const User = mongoose.model("User");
+const User = require("./UserModel"); // Import your external user model here
 
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
